df9aee3e4985c4903ff1c9e2b138a9fccfda5d67
jsshubham <shubham.joshi@thewitslab.com>
2023-06-14 19:02:33 +0530

    feat: setup the server

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { Request, Response } from 'express';
+import Logging from '../library/logging';
+import http from 'http';
+
+dotenv.config();
+
+const router = express();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+// const MONGO_URL = process.env.MONGO_URL || '';
+
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.cfnmian.mongodb.net/?retryWrites=true&w=majority`;
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
+
+export const config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    server: {
+        port: SERVER_PORT
+    }
+};
---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/logging';
+
+const router = express();
+
+/*Connect to mongo */
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Connected to mongodb');
+        startServer();
+    })
+    .catch((err) => {
+        Logging.error('Unable to connect to the Database: ');
+        Logging.error(err);
+    });
+
+// Only start the server if it MongoDB Connect
+const startServer = () => {
+    router.use((req, res, next) => {
+        Logging.info(`Incoming -> Method [${req.method}] - url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+        res.on('finish', () => {
+            // Log the response
+            Logging.info(`Incoming -> Method [${req.method}] - url: [${req.url}] - IP: [${req.socket.remoteAddress}] - status:[${res.statusCode}]`);
+        });
+        next();
+    });
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if (req.method == 'OPTION') {
+            res.header('Access-control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        }
+        next();
+    });
+
+    // Routes
+
+    // healthCheck
+    router.get('/ping', (req, res, next) => {
+        res.status(200).json({
+            message: 'Server is Working Fine'
+        });
+    });
+
+    //Error Handling
+
+    router.use((req, res, next) => {
+        const error = new Error('Not Found');
+        Logging.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+    http.createServer(router).listen(config.server.port, () => {
+        Logging.info(`Server is running on port ${config.server.port}`);
+    });
+};
